69f52a2bb336a0783c943ae0144fbb9c8e61c749
christynatalia <christynju@gmail.com>
2019-12-26 18:34:58 +0700

    nmew

--- runn.py
+++ runn.py
@@ -20,20 +20,22 @@ from setttings import Introduction
 
 import sys
 
-a = []
-b = []
-c = []
+#Make the lists for the history
+a = ['A = ']
+b = ['B = ']
+c = ['C = ']
 
+#Loop everytime the user type yes on the introduction
 while True:
     intro = Introduction()
     intromsg = intro.programintro()
     intromsg = intromsg.title()
     if intromsg == 'Yes':
         print("The formula is Ax^{2} + Bx + C")
-        hasil = Data()
-        hasilA = int(hasil.newNilaiA())
-        hasilB = int(hasil.newNilaiB())
-        hasilC = int(hasil.newNilaiC())
+        value = Data()
+        valueA = int(value.newNilaiA())
+        valueB = int(value.newNilaiB())
+        valueC = int(value.newNilaiC())
     elif intromsg == 'No':
         print("bye")
         sys.exit()
@@ -42,53 +44,53 @@ while True:
        sys.exit()
        
        
-    if hasilA != 0:
+    if valueA != 0:
         def discriminant():
-            global nilaidiskriminan
-            nilaidiskriminan =(((hasilB**2) - (4*hasilA*hasilC)))
-            return nilaidiskriminan
+            global discriminantValue
+            discriminantValue =(((valueB**2) - (4*valueA*valueC)))
+            return discriminantValue
             
             
-        def akard():
-            if nilaidiskriminan < 0 :
+        def roots():
+            if discriminantValue < 0 :
                 print("can't count because it is imaginary(non-real)roots" )
                 sys.exit()
             else:
-                akard = math.sqrt(int(nilaidiskriminan))
+                roots = math.sqrt(int(discriminantValue))
                 global x1
                 global x2
-                x1 = round((-hasilB+ akard) / (2*hasilA),2)
-                x2 = round((-hasilB- akard) / (2*hasilA),2)
+                x1 = round((-valueB + roots) / (2*valueA),2)
+                x2 = round((-valueB - roots) / (2*valueA),2)
                 return x1,x2
         
         print("The discriminant of this graph is =",discriminant())
-        print("X1,X2 = ",akard())
+        print("X1,X2 = ",roots())
         
         def highestpoint():
             global highx
             global highy
-            highx = (-hasilB) / (2 * hasilA)
-            highy = -((hasilB**2) - (4*hasilA*hasilC))/ (4*hasilA)
+            highx = (-valueB) / (2 * valueA)
+            highy = -((valueB**2) - (4*valueA*valueC))/ (4*valueA)
             return highx,highy
         
         print("The highest point of this graph is = ",highestpoint())
         
         
         def conclusion():
-            if (nilaidiskriminan > 0 and hasilA > 0) or (nilaidiskriminan > 0 and hasilA < 0):
-                value = ("The conclusion is = Grafik memotong sumbu x ")
-            elif (nilaidiskriminan == 0 and hasilA > 0) or (nilaidiskriminan == 0 and hasilA < 0):
-                value = ("The conclusion is = Grafik menyinggung sumbu x ")
-            elif (nilaidiskriminan < 0 and hasilA > 0) or (nilaidiskriminan < 0 and hasilA < 0):
-                value = ("The conclusion is garis tidak memotong sumbu x ")
-            return value
+            if (discriminantValue > 0 and valueA > 0) or (discriminantValue > 0 and valueA < 0):
+                conclmsg = ("The conclusion is = Grafik memotong sumbu x ")
+            elif (discriminantValue == 0 and valueA > 0) or (discriminantValue == 0 and valueA < 0):
+                conclmsg = ("The conclusion is = Grafik menyinggung sumbu x ")
+            elif (discriminantValue < 0 and valueA > 0) or (discriminantValue < 0 and valueA < 0):
+                conclmsg = ("The conclusion is garis tidak memotong sumbu x ")
+            return conclmsg
         
         print(conclusion())
         
         def history():
-            a.append(hasilA)
-            b.append(hasilB)
-            c.append(hasilC)
+            a.append(valueA)
+            b.append(valueB)
+            c.append(valueC)
             return a,b,c
         
         print("History = ",history())
@@ -111,15 +113,16 @@ while True:
         #axes.plot(a,b)
         #plt.show()
         
-    
+        
+        #Make the graph 
         x=numpy.linspace(float(x2),float(x1),50);
-        y=(hasilA *( x ** 2)) +(( x * hasilB ))+ hasilC
+        y=(valueA *( x ** 2)) +(( x * valueB ))+ valueC
         pyplot.plot(x,y);
         plt.scatter(float(highx),float(highy),s=10, c= "blue",label = 'HP=' + '('+ str(highx) + ',' + str(highy)+')')
-        plt.scatter(float(x1),float((hasilA *( x1 ** 2)) +(( x1 * hasilB ))+ hasilC), s = 20, c= 'red', label = 'x1('+ str(x1)+ str(')'))
-        plt.scatter(float(x2),float((hasilA *( x2 ** 2)) +(( x2 * hasilB ))+ hasilC), s = 20, c= 'black', label = 'x2('+ str(x2)+ str(')'))
+        plt.scatter(float(x1),float((valueA *( x1 ** 2)) +(( x1 * valueB ))+ valueC), s = 20, c= 'red', label = 'x1('+ str(x1)+ str(')'))
+        plt.scatter(float(x2),float((valueA *( x2 ** 2)) +(( x2 * valueB ))+ valueC), s = 20, c= 'black', label = 'x2('+ str(x2)+ str(')'))
         plt.legend()
-        pyplot.title("Parabola curve")
+        pyplot.title("Quadratic Equations Curve")
         pyplot.xlabel("x axis")
         pyplot.ylabel("y axis")
         pyplot.grid()
